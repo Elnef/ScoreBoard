acc0a26447a517b4af4313bf4f47d57fe8b2d000
Javier Ruiz <javier.ruiz@atos.net>
2022-08-26 16:37:31 +0200

    feat(#1): created initial library

---
+++ ScoreBoardManager/ScoreBoardManager/match.vb
@@ -0,0 +1,25 @@
+﻿Public Class match
+
+    'Starting date and time
+    Public Property dateMatch As DateTime
+
+    'Match Minute
+    Public Property minuteMatch As Integer
+
+    'Team names
+    Public Property homeTeam As String
+
+    Public Property awayTeam As String
+
+    'Scores
+    Public Property homeScore As Integer
+
+    Public Property awayScore As Integer
+
+    'Flag to match finished
+    Public Property isFinished As Boolean
+
+    'Flag to check if match is created correctly
+    Public Property isValid As Boolean
+
+End Class
