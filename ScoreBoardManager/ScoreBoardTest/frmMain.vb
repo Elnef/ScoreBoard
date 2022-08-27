df133e49c44d3e70681aca52ba1b884b1af9a040
Javier Ruiz <javier.ruiz@atos.net>
2022-08-27 11:50:32 +0200

    feat(#3): score board live is showed

--- ScoreBoardManager/ScoreBoardTest/frmMain.vb
+++ ScoreBoardManager/ScoreBoardTest/frmMain.vb
@@ -14,30 +14,24 @@
 
     End Sub
 
-    Private Sub BtnAddMatch_Click(sender As Object, e As EventArgs) Handles btnAddMatch.Click
-
-        Dim newMatch As ScoreBoardManager.Match
-
-
-
-    End Sub
-
     Private Sub ShowMatchesList()
 
-        ' Create the arrays of labels
+        ' Create the arrays of labels and textbox
         Dim labelHome() As Label = {Label1, Label2, Label3, Label4, Label5}
         Dim labelAway() As Label = {Label6, Label7, Label8, Label9, Label10}
+        Dim textHome() As TextBox = {TextBox1, TextBox2, TextBox3, TextBox4, TextBox5}
+        Dim textAway() As TextBox = {TextBox6, TextBox7, TextBox8, TextBox9, TextBox10}
 
         Dim index As Integer = 0
 
+        ' For each match update the Live score frame
         For Each score In scoreBoard
             labelHome(index).Text = score.MatchData.HomeTeam
             labelAway(index).Text = score.MatchData.AwayTeam
-            index = index + 1
+            textHome(index).Text = score.HomeScore
+            textAway(index).Text = score.AwayScore
+            index += 1
         Next
-
-        txt1.Text = scoreBoard(0).MatchData.HomeTeam
-
 
     End Sub
 
