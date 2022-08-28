fdfba1ac36f0ebd47bf62c67d20783fc6c1fbbb3
Javier Ruiz <javier.ruiz@atos.net>
2022-08-28 13:06:22 +0200

    fix(#5): modified library calling

--- ScoreBoardManager/ScoreBoardTest/modFunctions.vb
+++ ScoreBoardManager/ScoreBoardTest/modFunctions.vb
@@ -1,78 +1,79 @@
-﻿Module modFunctions
+﻿Imports ScoreBoardManager
+Module modFunctions
 
     ' This is our global scoreBoard
-    Public scoreBoard As New List(Of ScoreBoardManager.Match)
+    Public scoreBoard As New List(Of Match)
 
     ' This is a simulated matches list
-    Public matchesScheduled As New List(Of ScoreBoardManager.MatchScheduled) From
+    Public matchesScheduled As New List(Of MatchScheduled) From
         {
-            New ScoreBoardManager.MatchScheduled With {.DateMatch = "2022-08-26T15:00:00Z", .HomeTeam = "Mexico", .AwayTeam = "Canada"},
-            New ScoreBoardManager.MatchScheduled With {.DateMatch = "2022-08-26T16:00:00Z", .HomeTeam = "Spain", .AwayTeam = "Brazil"},
-            New ScoreBoardManager.MatchScheduled With {.DateMatch = "2022-08-26T17:00:00Z", .HomeTeam = "Germany", .AwayTeam = "France"},
-            New ScoreBoardManager.MatchScheduled With {.DateMatch = "2022-08-26T18:00:00Z", .HomeTeam = "Uruguay", .AwayTeam = "Italy"},
-            New ScoreBoardManager.MatchScheduled With {.DateMatch = "2022-08-26T19:00:00Z", .HomeTeam = "Argentina", .AwayTeam = "Australia"}
+            New MatchScheduled With {.DateMatch = "2022-08-26T15:00:00Z", .HomeTeam = "Mexico", .AwayTeam = "Canada"},
+            New MatchScheduled With {.DateMatch = "2022-08-26T16:00:00Z", .HomeTeam = "Spain", .AwayTeam = "Brazil"},
+            New MatchScheduled With {.DateMatch = "2022-08-26T17:00:00Z", .HomeTeam = "Germany", .AwayTeam = "France"},
+            New MatchScheduled With {.DateMatch = "2022-08-26T18:00:00Z", .HomeTeam = "Uruguay", .AwayTeam = "Italy"},
+            New MatchScheduled With {.DateMatch = "2022-08-26T19:00:00Z", .HomeTeam = "Argentina", .AwayTeam = "Australia"}
         }
 
     Public eventIndex As Integer
 
-    Public matchEventList As New List(Of ScoreBoardManager.MatchEvent) From
+    Public matchEventList As New List(Of MatchEvent) From
         {
-            New ScoreBoardManager.MatchEvent With {.EventID = 1, .Team = "Mexico"},
-            New ScoreBoardManager.MatchEvent With {.EventID = 3, .Team = "Canada"},
-            New ScoreBoardManager.MatchEvent With {.EventID = 3, .Team = "Canada"},
-            New ScoreBoardManager.MatchEvent With {.EventID = 1, .Team = "Spain"},
-            New ScoreBoardManager.MatchEvent With {.EventID = 3, .Team = "Spain"},
-            New ScoreBoardManager.MatchEvent With {.EventID = 3, .Team = "Spain"},
-            New ScoreBoardManager.MatchEvent With {.EventID = 3, .Team = "Canada"},
-            New ScoreBoardManager.MatchEvent With {.EventID = 3, .Team = "Spain"},
-            New ScoreBoardManager.MatchEvent With {.EventID = 1, .Team = "Germany"},
-            New ScoreBoardManager.MatchEvent With {.EventID = 3, .Team = "Brazil"},
-            New ScoreBoardManager.MatchEvent With {.EventID = 3, .Team = "France"},
-            New ScoreBoardManager.MatchEvent With {.EventID = 3, .Team = "Canada"},
-            New ScoreBoardManager.MatchEvent With {.EventID = 3, .Team = "Sevilla"},
-            New ScoreBoardManager.MatchEvent With {.EventID = 3, .Team = "Brazil"},
-            New ScoreBoardManager.MatchEvent With {.EventID = 3, .Team = "Spain"},
-            New ScoreBoardManager.MatchEvent With {.EventID = 3, .Team = "Germany"},
-            New ScoreBoardManager.MatchEvent With {.EventID = 3, .Team = "Canada"},
-            New ScoreBoardManager.MatchEvent With {.EventID = 2, .Team = "Canada"},
-            New ScoreBoardManager.MatchEvent With {.EventID = 255, .Team = "Canada"},
-            New ScoreBoardManager.MatchEvent With {.EventID = 3, .Team = "Spain"},
-            New ScoreBoardManager.MatchEvent With {.EventID = 3, .Team = "Spain"},
-            New ScoreBoardManager.MatchEvent With {.EventID = 3, .Team = "Spain"},
-            New ScoreBoardManager.MatchEvent With {.EventID = 3, .Team = "France"},
-            New ScoreBoardManager.MatchEvent With {.EventID = 3, .Team = "Spain"},
-            New ScoreBoardManager.MatchEvent With {.EventID = 1, .Team = "Uruguay"},
-            New ScoreBoardManager.MatchEvent With {.EventID = 3, .Team = "Spain"},
-            New ScoreBoardManager.MatchEvent With {.EventID = 3, .Team = "Betis"},
-            New ScoreBoardManager.MatchEvent With {.EventID = 3, .Team = "Spain"},
-            New ScoreBoardManager.MatchEvent With {.EventID = 2, .Team = "Spain"},
-            New ScoreBoardManager.MatchEvent With {.EventID = 3, .Team = "Uruguay"},
-            New ScoreBoardManager.MatchEvent With {.EventID = 3, .Team = "Uruguay"},
-            New ScoreBoardManager.MatchEvent With {.EventID = 3, .Team = "Uruguay"},
-            New ScoreBoardManager.MatchEvent With {.EventID = 3, .Team = "Italy"},
-            New ScoreBoardManager.MatchEvent With {.EventID = 3, .Team = "Uruguay"},
-            New ScoreBoardManager.MatchEvent With {.EventID = 3, .Team = "Uruguay"},
-            New ScoreBoardManager.MatchEvent With {.EventID = 3, .Team = "Germany"},
-            New ScoreBoardManager.MatchEvent With {.EventID = 3, .Team = "Italy"},
-            New ScoreBoardManager.MatchEvent With {.EventID = 3, .Team = "Italy"},
-            New ScoreBoardManager.MatchEvent With {.EventID = 2, .Team = "Germany"},
-            New ScoreBoardManager.MatchEvent With {.EventID = 3, .Team = "Italy"},
-            New ScoreBoardManager.MatchEvent With {.EventID = 1, .Team = "Argentina"},
-            New ScoreBoardManager.MatchEvent With {.EventID = 3, .Team = "Italy"},
-            New ScoreBoardManager.MatchEvent With {.EventID = 3, .Team = "Argentina"},
-            New ScoreBoardManager.MatchEvent With {.EventID = 3, .Team = "Italy"},
-            New ScoreBoardManager.MatchEvent With {.EventID = 3, .Team = "Argentina"},
-            New ScoreBoardManager.MatchEvent With {.EventID = 3, .Team = "Uruguay"},
-            New ScoreBoardManager.MatchEvent With {.EventID = 2, .Team = "Uruguay"},
-            New ScoreBoardManager.MatchEvent With {.EventID = 3, .Team = "Australia"},
-            New ScoreBoardManager.MatchEvent With {.EventID = 2, .Team = "Argentina"},
-            New ScoreBoardManager.MatchEvent With {.EventID = 2, .Team = "Argentina"}
+            New MatchEvent With {.EventID = 1, .Team = "Mexico"},
+            New MatchEvent With {.EventID = 3, .Team = "Canada"},
+            New MatchEvent With {.EventID = 3, .Team = "Canada"},
+            New MatchEvent With {.EventID = 1, .Team = "Spain"},
+            New MatchEvent With {.EventID = 3, .Team = "Spain"},
+            New MatchEvent With {.EventID = 3, .Team = "Spain"},
+            New MatchEvent With {.EventID = 3, .Team = "Canada"},
+            New MatchEvent With {.EventID = 3, .Team = "Spain"},
+            New MatchEvent With {.EventID = 1, .Team = "Germany"},
+            New MatchEvent With {.EventID = 3, .Team = "Brazil"},
+            New MatchEvent With {.EventID = 3, .Team = "France"},
+            New MatchEvent With {.EventID = 3, .Team = "Canada"},
+            New MatchEvent With {.EventID = 3, .Team = "Sevilla"},
+            New MatchEvent With {.EventID = 3, .Team = "Brazil"},
+            New MatchEvent With {.EventID = 3, .Team = "Spain"},
+            New MatchEvent With {.EventID = 3, .Team = "Germany"},
+            New MatchEvent With {.EventID = 3, .Team = "Canada"},
+            New MatchEvent With {.EventID = 2, .Team = "Canada"},
+            New MatchEvent With {.EventID = 255, .Team = "Canada"},
+            New MatchEvent With {.EventID = 3, .Team = "Spain"},
+            New MatchEvent With {.EventID = 3, .Team = "Spain"},
+            New MatchEvent With {.EventID = 3, .Team = "Spain"},
+            New MatchEvent With {.EventID = 3, .Team = "France"},
+            New MatchEvent With {.EventID = 3, .Team = "Spain"},
+            New MatchEvent With {.EventID = 1, .Team = "Uruguay"},
+            New MatchEvent With {.EventID = 3, .Team = "Spain"},
+            New MatchEvent With {.EventID = 3, .Team = "Betis"},
+            New MatchEvent With {.EventID = 3, .Team = "Spain"},
+            New MatchEvent With {.EventID = 2, .Team = "Spain"},
+            New MatchEvent With {.EventID = 3, .Team = "Uruguay"},
+            New MatchEvent With {.EventID = 3, .Team = "Uruguay"},
+            New MatchEvent With {.EventID = 3, .Team = "Uruguay"},
+            New MatchEvent With {.EventID = 3, .Team = "Italy"},
+            New MatchEvent With {.EventID = 3, .Team = "Uruguay"},
+            New MatchEvent With {.EventID = 3, .Team = "Uruguay"},
+            New MatchEvent With {.EventID = 3, .Team = "Germany"},
+            New MatchEvent With {.EventID = 3, .Team = "Italy"},
+            New MatchEvent With {.EventID = 3, .Team = "Italy"},
+            New MatchEvent With {.EventID = 2, .Team = "Germany"},
+            New MatchEvent With {.EventID = 3, .Team = "Italy"},
+            New MatchEvent With {.EventID = 1, .Team = "Argentina"},
+            New MatchEvent With {.EventID = 3, .Team = "Italy"},
+            New MatchEvent With {.EventID = 3, .Team = "Argentina"},
+            New MatchEvent With {.EventID = 3, .Team = "Italy"},
+            New MatchEvent With {.EventID = 3, .Team = "Argentina"},
+            New MatchEvent With {.EventID = 3, .Team = "Uruguay"},
+            New MatchEvent With {.EventID = 2, .Team = "Uruguay"},
+            New MatchEvent With {.EventID = 3, .Team = "Australia"},
+            New MatchEvent With {.EventID = 2, .Team = "Argentina"},
+            New MatchEvent With {.EventID = 2, .Team = "Argentina"}
         }
 
     ' Init score board from scheduled matches list
     Public Sub InitMatchListFromMathScheduled()
 
-        scoreBoard = ScoreBoardManager.ScoreBoardHandler.InitMatchList(matchesScheduled)
+        scoreBoard = ScoreBoardHandler.InitMatchList(matchesScheduled)
         ' Reset event index
         eventIndex = 0
 
@@ -82,7 +83,7 @@
     Public Sub SendEvent()
 
         If (eventIndex < matchEventList.Count) Then
-            scoreBoard = ScoreBoardManager.ScoreBoardHandler.EventHandler(matchEventList(eventIndex), scoreBoard)
+            scoreBoard = ScoreBoardHandler.EventHandler(matchEventList(eventIndex), scoreBoard)
             eventIndex += 1
         Else
             MsgBox("No more available events", MessageBoxIcon.Information)
